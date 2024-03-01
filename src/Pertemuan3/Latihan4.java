1a34d37ff796fbe3a62bd6b23ac2a626de0949d9
FawwasSabil25 <fawwas2573@gmail.com>
2024-03-01 13:09:47 +0700

    Mengerjakan tugas

---
+++ src/Pertemuan3/Latihan4.java
@@ -0,0 +1,25 @@
+public class Latihan4 {
+    public static void main(String[] args) {
+        
+        Lingkaran l1 = new Lingkaran(5);
+        Lingkaran l2 = l1;
+        Lingkaran l3 = new Lingkaran(7);
+
+        System.out.println(l1.getJari2());
+        System.out.println(l2.getJari2());
+        System.out.println(l3.getJari2());
+
+        l2 = null;
+
+        System.out.println(l1.getJari2());
+        if(l2 != null){
+            System.out.println(l2.getJari2());
+            /*
+             * Untuk mengatasi apakah variabel null atau tidak
+             * diperlukan pengkondisian untuk mengecek hal tersebut
+             */
+        }
+        System.out.println(l3.getJari2());
+
+    }
+}
